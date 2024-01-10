fcd1f077bb3eb05b0ba2b0478dda41d124f7b4ba
Rutuja96k <102164670+Rutuja96k@users.noreply.github.com>
2024-01-10 19:10:47 +0530

    Add files via upload

--- AbstractDemo.java
+++ AbstractDemo.java
@@ -1,40 +1,29 @@
+
 abstract class Demo
 {
     public int A;
     public int B;
-    
+
     public Demo()
     {
-        System.out.println("Inside Constaructor ");
-        this.A=0;
-        this.B=0;
+        System.out.println("Inside Constructor");
+        this.A = 0;
+        this.B = 0;
     }
-    abstract void Fun();
-    // in c++ virtual void Fun()=0;
+    abstract void fun();
+    // virtual void fun() = 0;
+
     void gun()
     {
-        System.out.println("inside gun");
+        System.out.println("Inside gun");
     }
 }
-class Hello extends Demo
-{
-    public Hello()
-    {
-        System.out.println("Inside Hello Constructor");
-    }
-    public void Fun()
-    {
-        System.out.println("Inside Fun of Hello
-        ");
-    }
-}
+
 class AbstractDemo
 {
-    public static void main(String Arg[])
+    public static void main(String arg[])
     {
-       // Demo obj = new Demo();
-       Hello hobj = new Hello();
-       hobj.Fun();
-       hobj.gun();
+        // Demo obj = new Demo();   Error
+        
     }
 }
